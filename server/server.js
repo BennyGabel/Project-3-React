46edd2287c0a5d089c2bb404f887df9a23e14a05
13 Miles <bennygabel@gmail.com>
2023-01-05 10:05:00 -0500

    Add /client/src/reportWebVitals  and rem out some code in server.js; still not able ot do npm start

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 // const mongoose = require('mongoose');     //    Already on      ./config/connection
 const db = require('./config/connection');
-const {Greene, Users} = require('./src/models');
+// const {Greene, Users} = require('./src/models');   // [BG] comment  out
 
-const routes = require('./src/routes');  // https://localhost
+// const routes = require('./src/routes');  // https://localhost   // [BG] comment  out
 const PORT = process.env.PORT || 3001;
 // const PORT = 3001;
 const app = express();
@@ -12,6 +12,8 @@ const activity = 'Greene Shop'
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+/* // [BG] BEG -comment  out
 app.get('/all-users', (req, res) => {
   Users.find({}, (err, result) => {
     if (err) {
@@ -31,7 +33,7 @@ app.get('/all-items', (req, res) => {
     }
   });
 });
-
+*/ // [BG] END -  comment  out
 //
 
 
