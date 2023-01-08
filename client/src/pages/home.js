54525fc543449bb1ad94e8b45ecb0abbee2c75c5
13 Miles <bennygabel@gmail.com>
2023-01-08 11:57:46 -0500

    Attempt to render items via GraphQl

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,18 +1,65 @@
 import React, { Component }  from 'react';
-// import ITEMS from './../components/data/items.json';
-
-const Home = () => {
-
+// import ITEMS from './../components/data/items.json';     [OLD]
+import { useQuery } from '@apollo/client';
 
 
-  return (
+// [OLD] 
+// const Home = () => {
 
-  <div className='react-container'> 
-      Home Landing Page
-  </div>
+//   return (
+
+//   <div className='react-container'> 
+//       Home Landing Page
+//   </div>
   
-  );
-}
+//   );
+// }
 
 // Export Function 
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+import ThoughtList from '../components/ItemList';
+// import ThoughtForm from '../components/ThoughtForm';
+
+import { QUERY_ITEMS_ALL } from '../utils/queries';
+
+const Home = () => {
+  const { loading, data } = useQuery(QUERY_ITEMS_ALL);
+  const itemcode  = data?.itemcode  || [];
+  const name      = data?.name      || [];
+  const brand     = data?.brand     || [];
+  const price     = data?.price     || [];
+  const inventory = data?.inventory || [];
+  const category  = data?.category  || [];
+  
+  return (
+    <main>
+      <div className="flex-row justify-center">
+        {/* <div
+          className="col-12 col-md-10 mb-3 p-3"
+          style={{ border: '1px dotted #1a1a1a' }}
+        >
+          <ItemList />
+        </div> */}
+        <div className="col-12 col-md-8 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ItemList
+              items={itemcode}
+              name ={name}
+              brand={brand}
+              price={price}
+              inventory={inventory}
+              category={category}
+              title="Some Feed for item(s)..."
+            />
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
+
 export default Home
