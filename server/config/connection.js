6ae961e3e318df5723cf360d2fb51076d2bbfe9b
podasin <podasin@gmail.com>
2023-01-16 09:20:59 -0500

    heroku.. Application error..

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/greeneshop', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/greeneShop', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
