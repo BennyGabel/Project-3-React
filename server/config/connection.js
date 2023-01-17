a59974a1c17f6e950cfa3d570d7db2952ec630df
podasin <podasin@gmail.com>
2023-01-16 23:13:11 -0500

    typo in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhose:27017/greeneShop', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/greeneshop', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
